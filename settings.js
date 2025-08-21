7d68ebeafd043a755e3242785d0bac8013ea55b5
DemonFBI🍁... <maximeyuehan888@gmail.com>
2025-08-21 07:26:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~277f8f0bbff3cf7572550a9697ee0d97f7e1d2fc", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
